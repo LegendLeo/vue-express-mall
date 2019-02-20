0fd84b063b44a0d36937ef01a734de1de338a2ce
Leo <hqyx45@qq.com>
2019-02-20 23:58:58 +0800

    :sparkles: 增加全局变量

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path')
+
 module.exports = {
   devServer: {
     proxy: 'http://localhost:3000'
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, './src/variables.less')
+      ]
+    }
   }
 }
