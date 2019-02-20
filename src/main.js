72590cdc885089e39d35d004580b4f2a51aa7df0
huqing1473 <huqing1473@emao.com>
2019-02-20 18:37:13 +0800

    :sparkles: 增加lazyload插件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
 import './plugins/axios'
 import './plugins/element'
 
 Vue.config.productionTip = false
+Vue.use(VueLazyload, {
+  loading: '/static/images/loading.svg'
+})
 
 new Vue({
   router,
