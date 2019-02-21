e415d537763e1010049f4fadd9fce3828ba0c6fc
Leo <hqyx45@qq.com>
2019-02-22 00:10:16 +0800

    :zap: 优化mock数据

--- server/app.js
+++ server/app.js
@@ -3,7 +3,14 @@ const app = express()
 const goodsList = require('./mock/goodsList.json')
 
 app.get('/goods', function (req, res) {
-  res.json(goodsList)
+  const { type } = req.query
+  const data = Object.assign({}, goodsList)
+  data.data = data.data.filter(item => item.type === type)
+  if (data.data.length === 0) {
+    res.json(goodsList)
+  } else {
+    res.end(JSON.stringify(data))
+  }
 })
 
 app.listen(3000, function () {
