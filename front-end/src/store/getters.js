8cff75d66e4374e92ce24b5a013170815500c801
Leo <hqyx45@qq.com>
2019-03-12 00:28:56 +0800

    :sparkles: 增加用户相关vuex数据管理

--- front-end/src/store/getters.js
+++ front-end/src/store/getters.js
@@ -1,5 +1,6 @@
 const getters = {
   loginStatus: state => state.user.loginStatus,
+  username: state => state.user.userInfo.username,
   cartList: state => state.shoppingCart.cartList,
   cartListCount: state => state.shoppingCart.cartList.length
 }
--- front-end/src/store/modules/user.js
+++ front-end/src/store/modules/user.js
@@ -1,6 +1,19 @@
 const user = {
   state: {
-    loginStatus: localStorage.getItem('token')
+    loginStatus: localStorage.getItem('token'),
+    userInfo: {
+      username: localStorage.getItem('username')
+    }
+  },
+  mutations: {
+    TOGGLE_LOGIN_STATUS (state, token) {
+      state.loginStatus = token
+      localStorage.setItem('token', token)
+    },
+    CHANGE_USERNAME ({ userInfo }, name) {
+      userInfo.username = name
+      localStorage.setItem('username', name)
+    }
   }
 }
 
