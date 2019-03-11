1813df04b7f23b78f203a2e181fa77eac7c0ec36
Leo <hqyx45@qq.com>
2019-03-12 00:27:23 +0800

    :zap: 优化user数据结构

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -4,7 +4,29 @@ const Schema = mongoose.Schema
 const UserSchema =  new Schema({
   username: { type: String, required: true, max: 50 },
   password: { type: String, required: true, max: 50 },
-  role: String
+  role: {
+    type: String,
+    default: 'user'
+  },
+  meta: {
+    createTime: {
+      type: Date,
+      default: Date.now()
+    },
+    updateTime: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+
+UserSchema.pre('save', function (next) {
+  if (this.isNew) {
+    this.meta.createTime = this.meta.updateTime = Date.now()
+  } else {
+    this.meta.updateTime = Date.now()
+  }
+  next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
