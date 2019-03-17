f212983d849e1971554575280fa68822d1f6c18a
Leo <hqyx45@qq.com>
2019-03-18 00:16:48 +0800

    :bug: 修复描述不显示

--- back-end/models/product.js
+++ back-end/models/product.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const ProductSchema = new Schema({
   name: { type: String, required: true, max: 120 },
+  description: { type: String, max: 500 },
   price: { type: Number, required: true },
   oldPrice: { type: Number, required: true },
   poster: { type: String, required: true },
