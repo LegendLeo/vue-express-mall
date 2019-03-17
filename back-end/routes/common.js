bb4e4f080f996a3fade697eaf74e0afaa71182eb
Leo <hqyx45@qq.com>
2019-03-17 17:14:41 +0800

    :sparkles: 新增上传图片接口

---
+++ back-end/routes/common.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const fse = require('fs-extra')
+const multer = require('multer')
+const uploader = multer({ dest: './back-end/upload/temp/' })
+const { response, authToken, formatTime } = require('../utils')
+
+// 上传文件
+router.post('/upload', authToken, uploader.any(), function(req, res) {
+  const file = req.files[0]
+  // 处理文件名
+  const now = new Date()
+  const filename = formatTime(now, 'yyyyMMdd') + '-' + file.originalname
+  const fileType = file.mimetype.split('/')[0]
+  const fileDes = `./back-end/upload/${fileType}/${filename}`
+  fse.readFile(file.path).then(data => {
+    fse
+      .outputFile(fileDes, data)
+      .then(() => {
+        res.send(
+          response.success('上传成功！', {
+            filename,
+            url: `/upload/${fileType}/${filename}`
+          })
+        )
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+})
+
+module.exports = router
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,11 @@
 const user = require('./user')
 const goods = require('./goods')
 const product = require('./product')
+const common = require('./common')
 
 module.exports = {
   user,
   goods,
-  product
+  product,
+  common
 }
