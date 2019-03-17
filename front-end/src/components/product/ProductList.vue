99126191ea19c1c182b41170b5c45428538694b8
Leo <hqyx45@qq.com>
2019-03-17 21:05:42 +0800

    :zap: 优化组件结构

--- front-end/src/components/product/ProductList.vue
+++ front-end/src/components/product/ProductList.vue
@@ -1,20 +1,69 @@
 <template>
-  <el-row :gutter="20">
-    <product-list-item
-      v-for="item in data"
-      :key="item._id"
-      :item="item"
-    ></product-list-item>
-  </el-row>
+  <div class="wrapper">
+    <div class="handle-bar">
+      <div class="bar-left">
+        <span>分类：</span>
+        <el-select v-model="type"
+          size="small"
+          @change="changeListType">
+          <el-option v-for="item in categories"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"></el-option>
+        </el-select>
+      </div>
+    </div>
+    <el-row :gutter="15">
+      <product-list-item v-for="item in productList"
+        :key="item._id"
+        :item="item"></product-list-item>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import ProductListItem from '@/components/product/ProductListItem'
+import { getProductList } from '@/api/product'
 
 export default {
-  name: 'List',
-  props: {
-    data: Array
+  name: 'ProductList',
+  data () {
+    return {
+      type: 'all',
+      productList: [],
+      categories: [
+        {
+          label: '全部',
+          value: 'all'
+        },
+        {
+          label: '家用',
+          value: 'home'
+        },
+        {
+          label: '配件',
+          value: 'fitting'
+        },
+        {
+          label: '出行',
+          value: 'travel'
+        }
+      ]
+    }
+  },
+  created () {
+    this.getProduct()
+  },
+  methods: {
+    getProduct (params) {
+      getProductList(params).then(res => {
+        const data = res.data
+        this.productList = data
+      })
+    },
+    changeListType () {
+      this.getProduct({ type: this.type })
+    }
   },
   components: {
     ProductListItem
@@ -22,5 +71,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.handle-bar {
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 15px;
+}
 </style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -2,8 +2,7 @@
   <el-main>
     <breadcrumb></breadcrumb>
     <div class="container">
-      <handle-bar @change="changeListType"></handle-bar>
-      <product-list :data="productList"></product-list>
+      <product-list></product-list>
     </div>
   </el-main>
 </template>
@@ -11,34 +10,12 @@
 <script>
 import Breadcrumb from '@/components/layout/Breadcrumb'
 import ProductList from '@/components/product/ProductList'
-import HandleBar from '@/components/product/HandleBar'
-import { getProductList } from '@/api/product'
 
 export default {
   name: 'home',
-  data () {
-    return {
-      productList: []
-    }
-  },
-  created () {
-    this.getProduct()
-  },
-  methods: {
-    getProduct (params) {
-      getProductList(params).then(res => {
-        const data = res.data
-        this.productList = data
-      })
-    },
-    changeListType (type) {
-      this.getProduct({ type })
-    }
-  },
   components: {
     Breadcrumb,
-    ProductList,
-    HandleBar
+    ProductList
   }
 }
 </script>
