ff5c64c7c4165e4efea9c138c76be5746eece6db
Leo <hqyx45@qq.com>
2019-03-18 23:48:35 +0800

    :zap: 优化响应提示文案

--- back-end/utils/response.js
+++ back-end/utils/response.js
@@ -1,18 +1,18 @@
 module.exports.response = {
-  success: function (msg = '成功', data = []) {
+  success: function (msg = '操作成功', data = []) {
     return {
       errCode: 0,
       data: data,
       msg: msg
     }
   },
-  error: function (msg = '请求参数有误，操作失败！') {
+  error: function (msg = '操作失败！') {
     return {
       errCode: 110,
       msg
     }
   },
-  expire: function (msg = '身份过期，请重新登录') {
+  expire: function (msg = '身份过期，请重新登录!') {
     return {
       errCode: 120,
       msg
